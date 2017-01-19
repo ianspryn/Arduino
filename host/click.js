130275f5feb0d9107fa5c3e3ea8e2453e66d927d
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2017-01-18 21:44:50 -0500

    Major Overhaul, including toggled switches
    Replaced buttons with switches

--- host/click.js
+++ host/click.js
@@ -1,24 +1,75 @@
 (function($) {
     $(function() {
-        $('a')  
-        
-        $('a').click(function() {
-            var link = $(this);
-        
+        function callBkg(url) {
             $.ajax({
                 type: 'GET',
-                url: link.prop('href'),
-                success: function(html, textStatus) {
-                    var text = link.text();
-                    link.text('Success!');
-                    
-                    setTimeout(function() {
-                        link.text(text);
-                    }, 3000);
-                }
+                url: url
             });
+        }
 
-            return false;
+        $('#hall').click(function() {
+            var checkbox = $(this);
+            var label = $('.hall-text');
+
+            
+            label.text(checkbox.is(':checked') ? "Hall on" : "Hall off");
+            
+            if (label.text() == "Hall off") {
+                callBkg("/?hallOff");
+            } else if (label.text() == "Hall on") {
+                callBkg("/?hallOn");
+            }
+        });
+        $('#hall-front').click(function() {
+            var checkbox = $(this);
+            var label = $('.hall-front-text');
+
+            
+            label.text(checkbox.is(':checked') ? "Hall Front on" : "Hall Front off");
+            
+            if (label.text() == "Hall Front off") {
+                callBkg("/?frontOff");
+            } else if (label.text() == "Hall Front on") {
+                callBkg("/?frontOn");
+            }
+        });
+        $('#hall-back').click(function() {
+            var checkbox = $(this);
+            var label = $('.hall-back-text');
+
+            
+            label.text(checkbox.is(':checked') ? "Hall Back on" : "Hall Back off");
+            
+            if (label.text() == "Hall Back off") {
+                callBkg("/?backOff");
+            } else if (label.text() == "Hall Back on") {
+                callBkg("/?backOn");
+            }
+        });
+        $('#walk').click(function() {
+            var checkbox = $(this);
+            var label = $('.walk-text');
+
+            
+            label.text(checkbox.is(':checked') ? "Walk on" : "Walk off");
+            
+            if (label.text() == "Walk off") {
+                callBkg("/?walkOff");
+            } else if (label.text() == "Walk on") {
+                callBkg("/?walkOn");
+            }
+        });
+        $('#lamp').click(function() {
+            var checkbox = $(this);
+            var label = $('.lamp-text');
+            
+            label.text(checkbox.is(':checked') ? "Lamp Post on" : "Lamp Post off");
+            
+            if (label.text() == "Lamp Post off") {
+                callBkg("/?postOff");
+            } else if (label.text() == "Lamp Post on") {
+                callBkg("/?postOn");
+            }
         });
     });
 })(jQuery);
